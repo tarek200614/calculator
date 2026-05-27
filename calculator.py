3d6c6af98d59476cb8da51456a18019711d8849e
tarek200614 <meghariabderrhmanetarek@gmail.com>
2026-05-27 15:45:40 +0200

    ajout de noveau fonction

--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
 def calculer_tva(prix_ht: float) -> float:
     return prix_ht *1.20
 
-    
+def convertir_euros_dollars(montant_euros: float) -> float:
+    return montant_euros *1.08
+
 prix_ttc = calculer_tva(100)
 print(f"Prix TTC :{prix_ttc}")
